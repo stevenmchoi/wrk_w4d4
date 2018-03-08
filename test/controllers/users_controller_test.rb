edaca413ce144036a431aec9f15716be299d8e87
Steven Choi <steven.m.choi@gmail.com>
2018-03-08 15:08:48 -0800

    Rename controller files

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class SessionsControllerControllerTest < ActionDispatch::IntegrationTest
+class UsersControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
